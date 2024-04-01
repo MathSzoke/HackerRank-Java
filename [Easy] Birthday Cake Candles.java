1b68b5f128a8014face6fb8b38bc4ea86c3a739d
Matheus Henrique Szoke La Motta <matheus.szoke@logtrac.com.br>
2024-03-31 23:53:23 -0300

    Adding new Easily solved challenges

---
+++ [Easy] Birthday Cake Candles.java
@@ -0,0 +1,99 @@
+/*
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+candles = [4, 4, 1, 3]
+
+The maximum height candles are  units high. There are 2 of them, so return 2.
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+Input Format
+
+The first line contains a single integer, , the size of .
+The second line contains  space-separated integers, where each integer  describes the height of .
+
+Constraints
+
+- 1 <= n <= 10^5
+- 1 <= candles[i] <= 10^7
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+Candle heights are [3, 2, 1, 3]. The tallest candles are 3 units, and there are 2 of them.
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result
+{
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles)
+    {
+        Map<Integer, Integer> numberCount = new HashMap<>();
+        int maxCount = 0;
+
+        for (int n : candles)
+        {
+            int c = numberCount.getOrDefault(n, 0) + 1;
+            numberCount.put(n, c);
+            if (c > maxCount) maxCount = c;
+        }
+
+        return maxCount;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
