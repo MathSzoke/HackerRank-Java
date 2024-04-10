0084765e4e16ea21bd3ef686850c1e68beae02fd
MathSzoke <matheusszoke@gmail.com>
2024-04-10 15:50:42 -0300

    New challenge solved at 10-04-24

---
+++ [Easy] Stair Case.java
@@ -0,0 +1,92 @@
+/*
+Staircase detail
+
+This is a staircase of size :
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Function Description
+
+Complete the staircase function in the editor below.
+
+staircase has the following parameter(s):
+
+int n: an integer
+Print
+
+Print a staircase as described above.
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Constraints
+
+ .
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n = 6.
+*/
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        int sharp = 1;
+        do
+        {
+            // Este código usa o método repeat da classe String para criar uma string com n espaços em branco e sharp em #
+            // Este método Repeat funciona igual a um foreach, mas para repetição de strings como no exemplo abaixo.
+            System.out.println(" ".repeat(n - 1) + "#".repeat(sharp));
+            n--;
+            sharp++;
+        }
+        while(n != 0);
+    }
+
+}
+
+class Solution
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
